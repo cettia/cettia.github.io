5ef6b30aba991fff6d57680f15bbe0127f972e2a
Donghwan Kim <flowersinthesand@gmail.com>
2016-03-13 22:21:06 +0900

    Conform to new code style

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,10 @@
 $(document).foundation();
 $("#content :header").each(function() {
-    var $this = $(this);
-    var $link = $("<a />").attr("href", "#" + $this.attr('id')).text(String.fromCharCode("182"));
-    $this.append($link.hide()).hover(function() {
-        $link.show();
-    }, function() {
-        $link.hide();
-    });
+  var $this = $(this);
+  var $link = $("<a />").attr("href", "#" + $this.attr('id')).text(String.fromCharCode("182"));
+  $this.append($link.hide()).hover(function() {
+    $link.show();
+  }, function() {
+    $link.hide();
+  });
 });
