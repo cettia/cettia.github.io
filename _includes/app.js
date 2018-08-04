c2f568853ac16f05b07b795f02237595b68c27f4
Donghwan Kim <flowersinthesand@gmail.com>
2018-08-04 22:02:35 +0900

    Replace features with a tutorial summary

--- _includes/app.js
+++ _includes/app.js
@@ -1,5 +1,5 @@
 $(document).foundation();
-$("#content :header").each(function() {
+$("#content :header, #index h2, #index h3").each(function() {
   var $this = $(this);
   var $link = $("<a />").attr("href", "#" + $this.attr('id')).text(String.fromCharCode("182"));
   $this.append($link.hide()).hover(function() {
