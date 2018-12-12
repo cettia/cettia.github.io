ba67de73c1257993e3a661eea953f22a4e77d244
Donghwan Kim <flowersinthesand@gmail.com>
2018-12-13 01:33:34 +0900

    Simplify highlighting

--- _includes/app.js
+++ _includes/app.js
@@ -8,16 +8,8 @@ $("#content :header, #index h2, #index h3").each(function() {
     $link.hide();
   });
 });
-document.querySelectorAll("#example-tabs + .tabs-content pre.highlight > code, #example-tabs .accordion-content pre.highlight > code").forEach(elem => {
-  Array.from(elem.childNodes).reduce((result, current) => {
-    let arr = result[result.length - 1];
-    if (current.nodeType === Node.TEXT_NODE && current.textContent[0] === '\n') {
-      arr = [];
-      result.push(arr);
-    }
-    arr.push(current);
-    return result;
-  }, [[]])
-  .filter(arr => !arr.find(elem => /hts|wts|\/cettia/.test(elem.textContent)))
-  .forEach(arr => arr.filter(elem => elem.nodeType === Node.ELEMENT_NODE).forEach(elem => elem.classList.add("blurred")));
+$("#example-tabs + .tabs-content pre.highlight > code, #example-tabs .accordion-content pre.highlight > code").each(function() {
+  $(this).html($(this).html().split(/\n/).map(function (line) {
+    return /hts|wts/.test(line) ? "<span class='highlighted'>" + line + "</span>" : line;
+  }).join('\n'));
 });
